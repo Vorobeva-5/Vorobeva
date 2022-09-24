9993f521994e17e5f60954ae6004394bb06d2e83
Vorobeva <natic_48@mail.ru>
2022-09-24 10:39:11 +0300

    добавил файлы первого занятия

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Нellow")
+
+const ourPlanetName = "Земля";
+console.log(ourPlanetName);
+
+const userTime = "12 часов";
+console.log(userTime);
+
+
+let article = "количество статей";
+console.log(article);
+
+let payCASHorCARD = "оплата наличными или нет";
+console.log(payCASHorCARD);
+
+let fullName = "Воробьева Наталья Сергеевна";
+console.log(fullName);
+
+let firstName = "Наталья";
+console.log(firstName);
+
+let secondName = "Сергеевна";
+console.log(secondName);
