0cd57fe98cfd4c81845d7ac205c8573b3ffe7e74
Vorobeva <natic_48@mail.ru>
2022-09-24 13:07:25 +0300

    новая структура папок

---
+++ JS/index.js
@@ -0,0 +1,49 @@
+/*console.log("Нellow");
+
+const ourPlanetName = "Земля";
+console.log(ourPlanetName);
+
+const userTime = "12 часов";
+console.log(userTime);
+
+let article = "количество статей";
+console.log(article);
+
+let payCASHorCARD = "оплата наличными или нет";
+console.log(payCASHorCARD);
+
+let fullName = "Воробьева Наталья Сергеевна";
+console.log(fullName);
+
+let firstName = "Наталья";
+console.log(firstName);
+let secondName = "Сергеевна";
+console.log(secondName);*/
+
+/*const age = 18;
+
+const userData = `умножение: ${3*3}, сумма ${3+3}`;
+
+console.log(userData);
+
+const isUser=false;
+const isVisible=true;
+console.log(isUser);*/
+
+// let townName;
+// townName = "Москва";
+// console.log(townName);
+
+const userData = {
+   name: "Иван",
+   age: 25,
+};
+console.log(userData.age);
+
+//alert ("Hellow word");
+console.log(typeof userData.age);
+
+console.log(typeof userDatss);
+
+const num=123748589409403444455n;
+console.log(num);
